54bdec90daa75ff2eff044d3ef4ee84cc7e09615
Salladen <Salladen@users.noreply.github.com>
2022-01-31 23:13:45 +0100

    Driver class fixes: Adds rooms with random sizes at random locations. The rooms are added in way so that it never tries creating, or placing a room twice, making it much quicker than a brute-force random method. It now actually places rooms after creating them. It deletes all rooms that are marked as invalid by the Level.place() method.

--- lab2/Driver.java
+++ lab2/Driver.java
@@ -8,7 +8,6 @@ import java.awt.*;
 import java.util.*;
 
 public class Driver {
-
 	public int getRandomWithExclusion(Random rnd, int start, int end, int... exclude) {
 		int random = start + rnd.nextInt(end - start + 1 - exclude.length);
 		for (int ex : exclude) {
@@ -22,11 +21,12 @@ public class Driver {
 
 	public void run() {
 		Random random = new Random();
+		Level level = new Level();
 
 		int roomAmount = random.nextInt(5,26);
 
 		Room[] rooms = new Room[roomAmount];
-		int[][] requestedRooms = new int[roomAmount][roomAmount];
+		int[][] requestedRoomCords = new int[roomAmount][roomAmount];
 		ArrayList<java.awt.Color> colors = new ArrayList<>();
 		for (int red = 0; red <= 255; red++){
 			for (int green = 0; green <= 255; green++){
@@ -38,13 +38,62 @@ public class Driver {
 			}
 		}
 
-		for (int i = 0; i < roomAmount; i++){
-			for (int j = 0; j < roomAmount; j++){
-				int randomColorIndex = random.nextInt(colors.size());
+		int currentPlacingBounds = 30 * roomAmount;
+		ArrayList<int[]> availableRoomCords = new ArrayList<>();
 
-				int origin = i * 10;
-				int bound = (i == 0) ? 10 : i * 20;
-				rooms[i] = new Room(random.nextInt(origin, bound), random.nextInt(origin, bound), colors.remove(randomColorIndex));
+		for (int x = 0; x < currentPlacingBounds; x++){
+			for (int y = 0; y < currentPlacingBounds; y++){
+				availableRoomCords.add(new int[]{x,y});
+			}
+		}
+
+		for (int i = 0; i < roomAmount; i++){
+			if (((double) availableRoomCords.size() / currentPlacingBounds * currentPlacingBounds) <= 0.3){
+				for (int x = currentPlacingBounds; x < currentPlacingBounds * 2; x++){
+					for (int y = 0; y < Math.pow(currentPlacingBounds * 2,2); y++){
+						availableRoomCords.add(new int[]{x,y});
+					}
+				}
+			}
+
+			int randomColorIndex = random.nextInt(colors.size());
+			Color color = colors.remove(randomColorIndex);
+
+			int randomCoordinateIndex = random.nextInt(availableRoomCords.size());
+			int[] randomCoordinate = availableRoomCords.get(randomCoordinateIndex);
+
+			int searchSize = Math.min(availableRoomCords.size() - (randomCoordinateIndex + 1), 25);
+
+			boolean maxWidthNotFound = true;
+			boolean maxHeightNotFound = true;
+			int roomWidth;
+			int roomHeight;
+			int maxHeight = 25;
+			int maxWidth = 25;
+
+			for (int j = randomCoordinateIndex; j < searchSize - 1; j++) {
+				int[] currentCord = availableRoomCords.get(j);
+				int[] nextCord = availableRoomCords.get(j + 1);
+
+				if (nextCord[0] != currentCord[0] + 1 && maxWidthNotFound) {
+					maxWidth = currentCord[0] - randomCoordinate[0];
+					maxWidthNotFound = false;
+				}
+
+				if (nextCord[1] != currentCord[1] + 1 && maxHeightNotFound) {
+					maxHeight = currentCord[1] - randomCoordinate[1];
+					maxHeightNotFound = false;
+				}
+			}
+			roomWidth = random.nextInt(1, maxWidth + 1);
+			roomHeight = random.nextInt(1, maxHeight + 1);
+
+			rooms[i] = new Room(roomWidth, roomHeight, color);
+			requestedRoomCords[i] = randomCoordinate;
+			for (int x = requestedRoomCords[i][0]; x <= requestedRoomCords[i][0] + roomWidth; x++){
+				for (int y = requestedRoomCords[i][1]; y <= requestedRoomCords[i][1] + roomHeight; y++){
+					availableRoomCords.remove(new int[]{x,y});
+				}
 			}
 		}
 
@@ -80,7 +129,15 @@ public class Driver {
 				}
 			}
 		}
-		//TODO task 3.2
-		//TODO should program react if it gets "false" (maybe something for "true" too?) from "place"-method?
+
+		for (int room = 0; room < rooms.length; room++){
+			if (!(level.place(rooms[room], requestedRoomCords[room][0], requestedRoomCords[room][1]))){
+				rooms[room] = null;
+			}
+		}
+		//TODO Add code: Make sure that the first room is placed at 0,0. This is easily fixed
+		//TODO Add code: Make it so that every room is accessible i.e. all rooms are interconnect through other rooms
+		//TODO Refactor: Segment code into methods appropriately
+		//TODO Others: Add comments
 	}
 }
