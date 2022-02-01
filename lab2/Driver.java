a9f3bf7dbac7821819565ce96f50d7657b17a493
Salladen <Salladen@users.noreply.github.com>
2022-02-01 06:47:52 +0100

    Amend details: Changed the comment slightly
    Fixed TODOs:
    Add code: Make sure that the first room is placed at 0,0. This is easily fixed
    Add code: Make it so that every room is accessible i.e. all rooms are interconnect through other rooms
    Refactor: Segment code into methods appropriately
    
    Other changes to Driver class:
    Added step size to the colors palette of rooms so that rooms can be created with more distinct colors
    The enhanced switch statement inside in the getConnectionSuggestion() method was changed into a regular one. Which is why it has break statements now

--- lab2/Driver.java
+++ lab2/Driver.java
@@ -8,7 +8,7 @@ import java.awt.*;
 import java.util.*;
 
 public class Driver {
-	public int getRandomWithExclusion(Random rnd, int start, int end, int... exclude) {
+	public static int getRandomWithExclusion(Random rnd, int start, int end, int... exclude) {
 		int random = start + rnd.nextInt(end - start + 1 - exclude.length);
 		for (int ex : exclude) {
 			if (random < ex) {
@@ -19,27 +19,31 @@ public class Driver {
 		return random;
 	}
 
-	public void run() {
-		Random random = new Random();
-		Level level = new Level();
-
-		int roomAmount = random.nextInt(5,26);
-
-		Room[] rooms = new Room[roomAmount];
-		int[][] requestedRoomCords = new int[roomAmount][roomAmount];
+	public static ArrayList<java.awt.Color> getColors (int stepSize){
 		ArrayList<java.awt.Color> colors = new ArrayList<>();
-		for (int red = 0; red <= 255; red++){
-			for (int green = 0; green <= 255; green++){
+		for (int red = 0; red <= 255; red += stepSize){
+			for (int green = 0; green <= 255; green += stepSize){
 				{
-					for (int blue = 0; blue <= 255; blue++){
+					for (int blue = 0; blue <= 255; blue += stepSize){
 						colors.add(new Color(red, green , blue));
 					}
 				}
 			}
 		}
+		return colors;
+	}
 
-		int currentPlacingBounds = 30 * roomAmount;
+	public static void getRoomSuggestion(ArrayList<Color> colors, int roomAmount, Room[] rooms, int[][]requestedRoomCords, int maxHeight, int maxWidth){
+		Random random = new Random();
+
+		int currentPlacingBounds = 30 * roomAmount * (int) Math.pow(maxHeight + maxWidth, 2);
 		ArrayList<int[]> availableRoomCords = new ArrayList<>();
+
+		int randomColorIndex = random.nextInt(colors.size());
+		Color color = colors.remove(randomColorIndex);
+		int roomWidth = random.nextInt(1, maxWidth + 1);
+		int roomHeight = random.nextInt(1, maxHeight + 1);
+
 
 		for (int x = 0; x < currentPlacingBounds; x++){
 			for (int y = 0; y < currentPlacingBounds; y++){
@@ -47,17 +51,22 @@ public class Driver {
 			}
 		}
 
-		for (int i = 0; i < roomAmount; i++){
-			if (((double) availableRoomCords.size() / currentPlacingBounds * currentPlacingBounds) <= 0.3){
-				for (int x = currentPlacingBounds; x < currentPlacingBounds * 2; x++){
-					for (int y = 0; y < Math.pow(currentPlacingBounds * 2,2); y++){
+		rooms[0] = new Room(roomWidth, roomHeight, color);
+		requestedRoomCords[0] = new int[]{0, 0};
+		for (int x = requestedRoomCords[0][0]; x <= requestedRoomCords[0][0] + roomWidth; x++){
+			for (int y = requestedRoomCords[0][1]; y <= requestedRoomCords[0][1] + roomHeight; y++){
+				availableRoomCords.remove(new int[]{x,y});
+			}
+		}
+
+		for (int i = 1; i < roomAmount; i++){
+			if (((double) availableRoomCords.size() / (currentPlacingBounds * currentPlacingBounds)) <= 0.3){
+				for (int x = 0; x < currentPlacingBounds * 2; x++){
+					for (int y = currentPlacingBounds; y < currentPlacingBounds * 2; y++){
 						availableRoomCords.add(new int[]{x,y});
 					}
 				}
 			}
-
-			int randomColorIndex = random.nextInt(colors.size());
-			Color color = colors.remove(randomColorIndex);
 
 			int randomCoordinateIndex = random.nextInt(availableRoomCords.size());
 			int[] randomCoordinate = availableRoomCords.get(randomCoordinateIndex);
@@ -66,10 +75,6 @@ public class Driver {
 
 			boolean maxWidthNotFound = true;
 			boolean maxHeightNotFound = true;
-			int roomWidth;
-			int roomHeight;
-			int maxHeight = 25;
-			int maxWidth = 25;
 
 			for (int j = randomCoordinateIndex; j < searchSize - 1; j++) {
 				int[] currentCord = availableRoomCords.get(j);
@@ -85,6 +90,9 @@ public class Driver {
 					maxHeightNotFound = false;
 				}
 			}
+
+			randomColorIndex = random.nextInt(colors.size());
+			color = colors.remove(randomColorIndex);
 			roomWidth = random.nextInt(1, maxWidth + 1);
 			roomHeight = random.nextInt(1, maxHeight + 1);
 
@@ -96,48 +104,82 @@ public class Driver {
 				}
 			}
 		}
+	}
+
+	public static void getConnectionSuggestion(Room[] rooms){
+		Random random = new Random();
+
+		int roomAmount = rooms.length;
+
+		ArrayList<Integer> isolatedRooms = new ArrayList<>();
+		for (int i = 0; i < roomAmount; i++){
+			isolatedRooms.add(i);
+		}
 
 		for (int i = 0; i < roomAmount; i++){
 			int[] roomExclusion = new int[5];
 			roomExclusion[0] = i;
-			for (int j = 1; j <= 4; j++) {
+			isolatedRooms.remove(i);
+
+			for (int j = 1; j <= Math.min(4, roomAmount); j++) {
+				Room randomRoom;
+				if (isolatedRooms.size() > 0){
+					int randInt = random.nextInt(0, isolatedRooms.size());
+
+					randomRoom = rooms[isolatedRooms.get(randInt)];
+					roomExclusion[j] = isolatedRooms.remove(randInt);
+				}
+				else{
+					int randInt = getRandomWithExclusion(random, 0, roomAmount - 1, roomExclusion);
+
+					randomRoom = rooms[randInt];
+					roomExclusion[j] = randInt;
+					isolatedRooms.remove(randInt);
+				}
+
 				switch (j) {
-					case 1 -> {
-						int randInt = getRandomWithExclusion(random, 0, roomAmount - 1, roomExclusion);
-						Room randomRoom = rooms[getRandomWithExclusion(random, 0, roomAmount - 1, i)];
+					case 1:
 						rooms[i].connectNorthTo(randomRoom);
-						roomExclusion[j] = randInt;
-					}
-					case 2 -> {
-						int randInt = getRandomWithExclusion(random, 0, roomAmount - 1, roomExclusion);
-						Room randomRoom = rooms[randInt];
+						break;
+
+					case 2:
 						rooms[i].connectEastTo(randomRoom);
-						roomExclusion[j] = randInt;
-					}
-					case 3 -> {
-						int randInt = getRandomWithExclusion(random, 0, roomAmount - 1, roomExclusion);
-						Room randomRoom = rooms[randInt];
+						break;
+
+					case 3:
 						rooms[j].connectSouthTo(randomRoom);
-						roomExclusion[j] = randInt;
-					}
-					case 4 -> {
-						int randInt = getRandomWithExclusion(random, 0, roomAmount - 1, roomExclusion);
-						Room randomRoom = rooms[randInt];
+						break;
+
+					case 4:
 						rooms[i].connectWestTo(randomRoom);
-						roomExclusion[j] = randInt;
-					}
+						break;
 				}
 			}
 		}
+	}
 
+	public void run() {
+		Level level = new Level();
+		Random random = new Random();
+
+		ArrayList<java.awt.Color> colors = getColors(32);
+
+		int roomAmount = random.nextInt(5,26);
+		Room[] rooms = new Room[roomAmount];
+		int[][] requestedRoomCords = new int[roomAmount][roomAmount];
+
+		int maxWidth = 25;
+		int maxHeight = 25;
+
+		getRoomSuggestion(colors, roomAmount, rooms, requestedRoomCords, maxHeight, maxWidth);
+		getConnectionSuggestion(rooms);
+
+		// Place rooms if legal by leve.place()
 		for (int room = 0; room < rooms.length; room++){
 			if (!(level.place(rooms[room], requestedRoomCords[room][0], requestedRoomCords[room][1]))){
 				rooms[room] = null;
 			}
 		}
-		//TODO Add code: Make sure that the first room is placed at 0,0. This is easily fixed
-		//TODO Add code: Make it so that every room is accessible i.e. all rooms are interconnect through other rooms
-		//TODO Refactor: Segment code into methods appropriately
 		//TODO Others: Add comments
 	}
 }
