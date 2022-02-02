24a1b2908528009df8ab355e3a4847bcd03d791f
drcatjk <98048355+drcatjk@users.noreply.github.com>
2022-02-02 09:36:11 +0100

    Task 5 and some alternative code for Task 5; made the instance variables for the class Level private; added setChanged() and notifyObservers() calls in order to report the current state of the level to the observers

--- lab2/level/Level.java
+++ lab2/level/Level.java
@@ -5,13 +5,13 @@ import java.util.ArrayList;
 import java.util.Observable;
 
 public class Level extends Observable {
-	ArrayList<Integer> usedXCoordinates = new ArrayList<>();
+	private ArrayList<Integer> usedXCoordinates = new ArrayList<>();
 
-	ArrayList<Integer> usedYCoordinates = new ArrayList<>();
+	private ArrayList<Integer> usedYCoordinates = new ArrayList<>();
 
-	boolean hasStarted = false;
+	private boolean hasStarted = false;
 
-	Room currentLocation;
+	private Room currentLocation;
 
 	public Level() {
 	}
@@ -30,6 +30,8 @@ public class Level extends Observable {
 				usedXCoordinates.add(x);
 				usedYCoordinates.add(y);
 
+				setChanged();
+				notifyObservers();
 				return true;
 			}
 		}
@@ -38,6 +40,91 @@ public class Level extends Observable {
 	public void firstLocation(Room r) {
 		this.hasStarted = true;
 		this.currentLocation = r;
+
+		setChanged();
+		notifyObservers();
 	}
+
+	void changeRoomNorth() {
+		if (currentLocation.doorNorth != null) {
+			this.currentLocation = currentLocation.doorNorth;
+			setChanged();
+			notifyObservers();
+		}
+	}
+
+	void changeRoomEast() {
+		if (currentLocation.doorEast != null) {
+			this.currentLocation = currentLocation.doorEast;
+			setChanged();
+			notifyObservers();
+		}
+	}
+
+	void changeRoomSouth() {
+		if (currentLocation.doorSouth != null) {
+			this.currentLocation = currentLocation.doorSouth;
+			setChanged();
+			notifyObservers();
+		}
+	}
+
+	void changeRoomWest() {
+		if (currentLocation.doorWest != null) {
+			this.currentLocation = currentLocation.doorWest;
+			setChanged();
+			notifyObservers();
+		}
+	}
+
+	//Code that I wrote after getting confused by the wording in Task 5 but, since it looked uglier than what I
+	// first made, I just commented it away. Feel free to delete it if you want a cleaner class.
+/*	public void changeRoom(String direction) {
+		switch (direction) {
+			case "north": {
+				if(currentLocation.doorNorth == null) {
+					break;
+				} else {
+					this.currentLocation = currentLocation.doorNorth;
+					setChanged();
+					notifyObservers();
+					break;
+				}
+			}
+			case "east": {
+				if(currentLocation.doorEast == null) {
+					break;
+				} else {
+					this.currentLocation = currentLocation.doorEast;
+					setChanged();
+					notifyObservers();
+					break;
+				}
+			}
+			case "south": {
+				if(currentLocation.doorSouth == null) {
+					break;
+				} else {
+					this.currentLocation = currentLocation.doorSouth;
+					setChanged();
+					notifyObservers();
+					break;
+				}
+			}
+			case "west": {
+				if(currentLocation.doorWest == null) {
+					break;
+				} else {
+					this.currentLocation = currentLocation.doorWest;
+					setChanged();
+					notifyObservers();
+					break;
+				}
+			}
+			default: {
+				throw new IllegalArgumentException();
+			}
+		}
+	}*/
 
 }
